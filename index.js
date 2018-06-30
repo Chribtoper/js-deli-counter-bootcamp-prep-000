5f6dddabbef88757e1f6e9cadfd5314be21c8e23
Chribtoper <xcstolfax@gmail.com>
2018-06-30 08:27:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(line){
   if (line.length > 0){
-    return `Currently serving ${line[1]}`, line.pop(1);
+    return `Currently serving ${line.length}`, line.pop(line.length);
   }
   else {
     return "There is nobody waiting to be served!";
