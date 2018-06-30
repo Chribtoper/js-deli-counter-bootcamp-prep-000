939a053b523ca65e32cbcb90694a1b0ce3b4d2d4
Chribtoper <xcstolfax@gmail.com>
2018-06-30 08:12:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(){
   if (katzDeli.length === 0) return "There is nobody waiting to be served!";
-  
+  else {
+    
+  }
 }
 
 function currentLine(){
