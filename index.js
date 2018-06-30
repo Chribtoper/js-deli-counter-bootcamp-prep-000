f8245001c9f63b3db48b35fabe64d59539caf725
Chribtoper <xcstolfax@gmail.com>
2018-06-30 08:34:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,11 @@ function nowServing(line){
 }
 
 function currentLine(line){
-  if (line.length === 0) return "The line is currently empty";
-  else {
-for (let i = 0;i )  
+  if (line.length === 0){
+    return "The line is currently empty";
+} else {
+for (let i = 0; i < line.length; i++){
+  
+}  
 }
-    
-  }
 
