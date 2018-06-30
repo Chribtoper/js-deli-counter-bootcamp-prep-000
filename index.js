6a71ff4ecbf8c0ed2531fb3a1be0d1b19c1e5956
Chribtoper <xcstolfax@gmail.com>
2018-06-30 08:43:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function nowServing(line){
 }
 
 function currentLine(line){
+  var temp = [];
   if (line.length === 0){
     return "The line is currently empty";
 } else {
-  console.log("The line is currently:");
 for (let i = 0; i < line.length; i++){
   console.log(`${i+1}. ${line[i]},`);
     }
