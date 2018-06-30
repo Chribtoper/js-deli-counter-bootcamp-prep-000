3c53d7430b155ea8d1e2c70b57fbbe23323f617f
Chribtoper <xcstolfax@gmail.com>
2018-06-30 08:07:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name) 
-  console.log(`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
+  console.log(`Welcome, ${name}. You are number ${katzDeli.length + 1} in line.`)
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
 
