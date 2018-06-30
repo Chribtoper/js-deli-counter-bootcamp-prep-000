e56dcc24bf211d45276da5880613b84d35d69dfa
Chribtoper <xcstolfax@gmail.com>
2018-06-30 08:25:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, name){
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing(){
+function nowServing(line){
   if (katzDeli.length > 0){
     return `Currently serving ${katzDeli[0]}`, katzDeli.pop(0);
   }
